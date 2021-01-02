a2994fef8be6a3bf251896881c60d47028dc5c30
Matteo Angeloni <mattange@gmail.com>
2021-01-02 22:58:07 +0000

    Modified slightly gas_resistance/aqi calculations and added biases

--- bme680_homie/device_bme680.py
+++ bme680_homie/device_bme680.py
@@ -12,14 +12,20 @@ from homie.support.repeating_timer import Repeating_Timer
 class Device_BME680(Device_Base):
     
     _MAX_RESISTANCE = 100000
-    _MIN_RESISTANCE = 30000
+    _MIN_RESISTANCE = 5000
     _HUMIDITY_WEIGHT = 0.25
     _upd_interval = 60
     _ideal_rel_humidity = 40.
+    _sensor_settings = {
+            "temperature_bias": 0.,
+            "humidity_bias": 0.,
+            "pressure_bias": 0.
+            }
+    
 
 
     def __init__(self, device_id=None, name=None, 
-            homie_settings=None, mqtt_settings=None):
+            sensor_settings={}, homie_settings=None, mqtt_settings=None):
         super().__init__(device_id, name, homie_settings, mqtt_settings)
         
         # assign sensor
@@ -28,6 +34,7 @@ class Device_BME680(Device_Base):
         except IOError:
             sensor = bme680.BME680(bme680.I2C_ADDR_SECONDARY)
         #sensor.set_humidity_oversample(bme680.OS_2X)
+        sensor.set_humidity_oversample(bme680.OS_8X)
         #sensor.set_pressure_oversample(bme680.OS_4X)
         #sensor.set_temperature_oversample(bme680.OS_8X)
         #sensor.set_filter(bme680.FILTER_SIZE_3)
@@ -36,6 +43,7 @@ class Device_BME680(Device_Base):
         sensor.set_gas_heater_duration(150)
         sensor.select_gas_heater_profile(0)
         self._sensor = sensor
+        self._sensor_settings.update(sensor_settings)
 
 
         # assign Homie features to update/request information on demand
@@ -54,18 +62,25 @@ class Device_BME680(Device_Base):
                 value=self._ideal_rel_humidity, data_format="0:100")
         node_worker.add_property(ideal_rel_humidity)
 
+
         # assign Homie features to base sensor
         node_sensor = Node_Base(self, id="multi-sensor", name="Multiple sensor", type_="status")
         self.add_node(node_sensor)
 
         pressure_prop = Property_Pressure(node=node_sensor, unit="hPa")
         node_sensor.add_property(pressure_prop)
+        pressure_bias_prop = Property_Pressure(node=node_sensor, id="pressure-bias", unit="hPa", settable=False, value=self._sensor_settings["pressure_bias"])
+        node_sensor.add_property(pressure_bias_prop)
         self._pressure = pressure_prop
         humidity_prop = Property_Humidity(node=node_sensor)
         node_sensor.add_property(humidity_prop)
+        humidity_bias_prop = Property_Humidity(node=node_sensor, id="humidity-bias", settable=False, value=self._sensor_settings["humidity_bias"])
+        node_sensor.add_property(humidity_bias_prop)
         self._humidity = humidity_prop
         temperature_prop = Property_Temperature(node=node_sensor, unit="°C")
         node_sensor.add_property(temperature_prop)
+        temperature_bias_prop = Property_Temperature(node=node_sensor, id="temperature-bias", unit="°C", settable=False, value=self._sensor_settings["temperature_bias"])
+        node_sensor.add_property(temperature_bias_prop)
         self._temperature = temperature_prop
         gas_res_prop = Property_Float(node=node_sensor, id="gas-resistance", name="Gas resistance", settable=False, unit="Ohm")
         node_sensor.add_property(gas_res_prop)
@@ -81,14 +96,22 @@ class Device_BME680(Device_Base):
     def _get_core_data(self):
         if self._mqtt_connected:
             self._sensor.get_sensor_data()
-            self._pressure.value = self._sensor.data.pressure
-            self._temperature.value = self._sensor.data.temperature
-            self._humidity.value = self._sensor.data.humidity
+            self._pressure.value = self._sensor.data.pressure + self._sensor_settings["pressure_bias"]
+            self._temperature.value = self._sensor.data.temperature + self._sensor_settings["temperature_bias"]
+            self._humidity.value = self._sensor.data.humidity + self._sensor_settings["humidity_bias"]
             if self._sensor.data.heat_stable:
-                self._gas_resistance.value = self._sensor.data.gas_resistance
-                self._aqi.value = self._calculate_aqi(self._sensor.data.gas_resistance, self._sensor.data.humidity)
+                adj_resistance = sel._adj_gas_resistance(self._sensor.data.gas_resistance, self._sensor.data.humidity)
+                self._gas_resistance.value = adj_resistance
+                self._aqi.value = self._calculate_aqi(adj_resistance, self._sensor.data.humidity)
         else:
             pass
+
+    def _adj_gas_resistance(self, gas_resistance, humidity):
+        # based on observations, gas resistance is dependent on humidity more than temperature
+        # so i adjust for it
+        # https://forums.pimoroni.com/t/bme680-observed-gas-ohms-readings/6608/16
+        return math.exp(math.log(gas_resistance) + 0.04 * humidity)
+
 
     def _calculate_aqi(self, gas_resistance, humidity):
         if humidity < self._ideal_rel_humidity:
